c1c70d73d2ccc9fcd1e7393f84da9ad9905ecf1a
Goutte <antoine@goutenoir.com>
2020-08-20 14:13:28 +0200

    Fix AudioEffectRecord::get_recording() crashes from time to time
    Thx to @Goutte for C++ help =)
    
    A bit of cleanup.
    
    fix: Mutex is created in the wrong place. by @Goutte
    
    (merge blooper)
    
    Make AudioEffectRecord encode on its thread.
    
    fix: move mutex creation to class constructor by @Goutte
    
    Kudos to @Adrenesis again for finding the crash on windows.
    
    fix: AudioEffectRecord::get_recording() crashes from time to time by @Goutte
    
    The size of recording_data may change DURING the call to get_recording(),
    because another thread may write to it.
    Big kudos to @Adrenesis for sweating this one out.
    
    This commit deploys a Mutex to force sequential access to recording_data. by @Goutte
    
    Not sure about the impact on sound quality of this fix.
    Not sure about the usage of ifndef in the header.
    
    fix: AudioEffectRecord::get_recording() crashes from time to time by @Goutte
    
    The size of recording_data may change DURING the call to get_recording(),
    because another thread may write to it.
    Big kudos to @Adrenesis for sweating this one out.

--- servers/audio/effects/audio_effect_record.cpp
+++ servers/audio/effects/audio_effect_record.cpp
@@ -91,10 +91,47 @@ void AudioEffectRecordInstance::_io_store_buffer() {
 	AudioFrame *rb_buf = ring_buffer.ptrw();
 
 	while (to_read) {
+#ifndef NO_THREADS
+		mutex_recording_data->lock();
+#endif
 		AudioFrame buffered_frame = rb_buf[ring_buffer_read_pos & ring_buffer_mask];
-		recording_data.push_back(buffered_frame.l);
-		recording_data.push_back(buffered_frame.r);
+		if (base->format == AudioStreamSample::FORMAT_8_BITS) {
+			int8_t v = CLAMP((buffered_frame.l) * 128, -128, 127);
+			recording_data.push_back(v);
+			v = CLAMP((buffered_frame.r) * 128, -128, 127);
+			recording_data.push_back(v);
+		} else if (base->format == AudioStreamSample::FORMAT_16_BITS) {
+			int16_t v = CLAMP(buffered_frame.l * 32768, -32768, 32767);
+			recording_data.push_back(v & 0xFF);
+			recording_data.push_back((v >> 8) & 0xFF);
+			v = CLAMP(buffered_frame.r * 32768, -32768, 32767);
+			recording_data.push_back(v & 0xFF);
+			recording_data.push_back((v >> 8) & 0xFF);
+		} else if (base->format == AudioStreamSample::FORMAT_IMA_ADPCM) {
+			//byte interleave
+//			PoolVector<uint8_t> bleft;
+//			PoolVector<uint8_t> bright;
+//
+//			ResourceImporterWAV::_compress_ima_adpcm(buffered_frame.l, bleft);
+//			ResourceImporterWAV::_compress_ima_adpcm(buffered_frame.r, bright);
+//
+//			int dl = bleft.size();
+//
+//			PoolVector<uint8_t>::Read rl = bleft.read();
+//			PoolVector<uint8_t>::Read rr = bright.read();
+//
+//			for (int i = 0; i < dl; i++) {
+//				recording_data.push_back(rl[i]);
+//				recording_data.push_back(rr[i]);
+//			}
+			ERR_PRINT("Format IMA_ADPCM not implemented in Amethyst. (yet)");
+		} else {
+			ERR_PRINT("Format not implemented.");
+		}
 
+#ifndef NO_THREADS
+		mutex_recording_data->unlock();
+#endif
 		ring_buffer_read_pos++;
 		to_read--;
 	}
@@ -137,6 +174,10 @@ void AudioEffectRecordInstance::finish() {
 AudioEffectRecordInstance::~AudioEffectRecordInstance() {
 
 	finish();
+
+#ifndef NO_THREADS
+	memdelete(mutex_recording_data);
+#endif
 }
 
 Ref<AudioEffectInstance> AudioEffectRecord::instance() {
@@ -212,71 +253,21 @@ AudioStreamSample::Format AudioEffectRecord::get_format() const {
 }
 
 Ref<AudioStreamSample> AudioEffectRecord::get_recording() const {
-	AudioStreamSample::Format dst_format = format;
 	bool stereo = true; //forcing mono is not implemented
-
-	PoolVector<uint8_t> dst_data;
 
 	ERR_FAIL_COND_V(current_instance.is_null(), NULL);
 	ERR_FAIL_COND_V(current_instance->recording_data.size() == 0, NULL);
 
-	if (dst_format == AudioStreamSample::FORMAT_8_BITS) {
-		int data_size = current_instance->recording_data.size();
-		dst_data.resize(data_size);
-		PoolVector<uint8_t>::Write w = dst_data.write();
-
-		for (int i = 0; i < data_size; i++) {
-			int8_t v = CLAMP(current_instance->recording_data[i] * 128, -128, 127);
-			w[i] = v;
-		}
-	} else if (dst_format == AudioStreamSample::FORMAT_16_BITS) {
-		int data_size = current_instance->recording_data.size();
-		dst_data.resize(data_size * 2);
-		PoolVector<uint8_t>::Write w = dst_data.write();
-
-		for (int i = 0; i < data_size; i++) {
-			int16_t v = CLAMP(current_instance->recording_data[i] * 32768, -32768, 32767);
-			encode_uint16(v, &w[i * 2]);
-		}
-	} else if (dst_format == AudioStreamSample::FORMAT_IMA_ADPCM) {
-		//byte interleave
-		Vector<float> left;
-		Vector<float> right;
-
-		int tframes = current_instance->recording_data.size() / 2;
-		left.resize(tframes);
-		right.resize(tframes);
-
-		for (int i = 0; i < tframes; i++) {
-			left.set(i, current_instance->recording_data[i * 2 + 0]);
-			right.set(i, current_instance->recording_data[i * 2 + 1]);
-		}
-
-		PoolVector<uint8_t> bleft;
-		PoolVector<uint8_t> bright;
-
-		ResourceImporterWAV::_compress_ima_adpcm(left, bleft);
-		ResourceImporterWAV::_compress_ima_adpcm(right, bright);
-
-		int dl = bleft.size();
-		dst_data.resize(dl * 2);
-
-		PoolVector<uint8_t>::Write w = dst_data.write();
-		PoolVector<uint8_t>::Read rl = bleft.read();
-		PoolVector<uint8_t>::Read rr = bright.read();
-
-		for (int i = 0; i < dl; i++) {
-			w[i * 2 + 0] = rl[i];
-			w[i * 2 + 1] = rr[i];
-		}
-	} else {
-		ERR_PRINT("Format not implemented.");
-	}
-
 	Ref<AudioStreamSample> sample;
 	sample.instance();
-	sample->set_data(dst_data);
-	sample->set_format(dst_format);
+#ifndef NO_THREADS
+	current_instance->mutex_recording_data->lock();
+#endif
+	sample->set_data(current_instance->recording_data);
+#ifndef NO_THREADS
+	current_instance->mutex_recording_data->unlock();
+#endif
+	sample->set_format(format);
 	sample->set_mix_rate(AudioServer::get_singleton()->get_mix_rate());
 	sample->set_loop_mode(AudioStreamSample::LOOP_DISABLED);
 	sample->set_loop_begin(0);
